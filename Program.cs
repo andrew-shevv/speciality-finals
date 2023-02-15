38026323e7fcdcf3c8074aa77c17441a9a3aa7c4
Andrew <vishnyaishere@gmail.com>
2023-02-15 18:11:01 +0300

    program done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string[] array = {"Hello", "world", "!!!", ":-)", "<3"};
+
+string[] LessArray(string[] array) 
+{
+    string[] resultingArray = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            resultingArray[i] = array[i];
+        }
+    }
+
+    resultingArray = resultingArray.Where(i => !string.IsNullOrWhiteSpace(i)).ToArray();
+
+    return resultingArray;
+}
+
+string[] arrayLessThanFour = LessArray(array);
+
+Console.Write('[' + string.Join(" ", array) + ']' + " ==> " + '[' + string.Join(" ", arrayLessThanFour) + ']');
